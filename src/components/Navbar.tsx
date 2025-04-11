a21aa1f5099563729317bd75374cdbfa16041f2b
Sajjad Mohammadi Nejad <daram3118@gmail.com>
2025-04-11 14:49:38 +0330

    add build

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -64,7 +64,7 @@ export const Navbar = () => {
           {/* mobile */}
           <span className="flex md:hidden">
             <ModeToggle />
-
+            <ModeLang />
             <Sheet
               open={isOpen}
               onOpenChange={setIsOpen}
